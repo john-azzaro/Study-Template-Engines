f0ec5ac472c36423c776e5465f5e62b225c19df5
John Azzaro <jazzaro@temple.edu>
2019-10-07 10:18:04 -0700

    modifed server.js, added use specifications

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const path = require('path');                                                   
 
 const app = express();                                                             // initialize app by calling express and storing as "app"
 
-app.set('views', path.join(__dirname, 'views'));                                   // This tells pug where the template files will be. 
-app.set('view engine', 'pug')                                                      // this tells express to use the pug view "templating" engine
+app.set('views', path.join(__dirname, 'views'));                                   // This tells pug where the template files will be. (note: "set" means set this on all requests) 
+app.set('view engine', 'pug')                                                      // this tells express to use the pug view "templating" engine.
+
+// app.use(express.static(path.join(__dirname + '/public')));                      // USE WITH STATIC PAGES: This will allow static pages from the public folder (note: "use" means use this )
 
 app.get('/', function(req, res) {                                                  // for home route, render the index.pug file.
     res.render('index');
 });
 
-// app.use(express.static(path.join(__dirname + '/public')));                      // This will allow static pages from the public folder (for reference compared to pug)
-
-// app.get('/', function(req, res) {                                               // Tester route for server.
+// app.get('/', function(req, res) {                                               // USE FOR INITAL CONNECTION TEST: Test route for server.
 //     res.send('Hello there fellow human')
 // })
 
