f8ac6115863b2efd205856b0b83d6dd9aee196ea
John Azzaro <jazzaro@temple.edu>
2019-10-06 20:22:54 -0700

    added basic server

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');                 // load express module
+
+const app = express();                              // initialize app by calling express and storing as "app"
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log(`Your app is listening on ${process.env.PORT || 3000}...`);
+})
