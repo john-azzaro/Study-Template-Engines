d2bbd7a87242ae610b327759d0659014a64047fb
John Azzaro <jazzaro@temple.edu>
2019-10-06 20:26:20 -0700

    added dev dependency

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const express = require('express');                 // load express module
 const app = express();                              // initialize app by calling express and storing as "app"
 
 app.listen(process.env.PORT || 3000, function() {
-    console.log(`Your app is listening on ${process.env.PORT || 3000}...`);
+    console.log(`Your app is listening on ${process.env.PORT || 3000}...`);        // listen for client requests 
 })
