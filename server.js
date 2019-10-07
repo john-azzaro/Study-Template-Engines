54a23e7ea7f404ffc630cd43677770dc413bbf16
John Azzaro <jazzaro@temple.edu>
2019-10-06 21:21:42 -0700

    added pug, pug route, views, process notes

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const express = require('express');                 // load express module
-const path = require('path');                       // load path module for static html files demo
+const express = require('express');                                                // load express module
+const path = require('path');                                                      // load path module for static html files demo
 
-const app = express();                              // initialize app by calling express and storing as "app"
+const app = express();                                                             // initialize app by calling express and storing as "app"
 
-app.use(express.static(path.join(__dirname + '/public')));      // This will allow static pages from the public folder (for reference)
+app.set('views', path.join(__dirname, 'views'));                                   // This tells pug where the template files will be. 
+app.set('view engine', 'pug')                                                      // this tells express to use the pug view "templating" engine
 
-// app.get('/', function(req, res) {
+app.get('/', function(req, res) {                                                  // for home route, render the index.pug file.
+    res.render('index');
+});
+
+// app.use(express.static(path.join(__dirname + '/public')));                      // This will allow static pages from the public folder (for reference compared to pug)
+
+// app.get('/', function(req, res) {                                               // Tester route for server.
 //     res.send('Hello there fellow human')
 // })
 
