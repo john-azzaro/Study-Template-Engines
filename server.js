db10b405025f280f9bce89dc8fce809db355ab29
John Azzaro <jazzaro@temple.edu>
2019-10-06 20:52:12 -0700

    static page example, server updated, notes

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');                 // load express module
+const path = require('path');                       // load path module for static html files demo
 
 const app = express();                              // initialize app by calling express and storing as "app"
 
-app.get('/', function(req, res) {
-    res.send('Hello there fellow human')
-})
+app.use(express.static(path.join(__dirname + '/public')));      // This will allow static pages from the public folder (for reference)
+
+// app.get('/', function(req, res) {
+//     res.send('Hello there fellow human')
+// })
 
 app.listen(process.env.PORT || 3000, function() {
     console.log(`Your app is listening on ${process.env.PORT || 3000}...`);        // listen for client requests 
