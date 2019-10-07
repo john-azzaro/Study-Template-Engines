2e4e390e674c514cdf0aaeb6eb77e437c9d14b4a
John Azzaro <jazzaro@temple.edu>
2019-10-06 20:31:41 -0700

    installed pug dependency, GET route

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');                 // load express module
 
 const app = express();                              // initialize app by calling express and storing as "app"
 
+app.get('/', function(req, res) {
+    res.render()
+})
+
 app.listen(process.env.PORT || 3000, function() {
     console.log(`Your app is listening on ${process.env.PORT || 3000}...`);        // listen for client requests 
 })
