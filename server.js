02803b5747e8c93590f60aa0f03bff2f39be25a3
John Azzaro <jazzaro@temple.edu>
2019-10-06 20:37:10 -0700

    added additional notes, pakage.json, server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');                 // load express module
 const app = express();                              // initialize app by calling express and storing as "app"
 
 app.get('/', function(req, res) {
-    res.render()
+    res.send('Hello there fellow human')
 })
 
 app.listen(process.env.PORT || 3000, function() {
